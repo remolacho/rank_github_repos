d96ed5e1e24b4b4cac7c9e43ae70f5e56c31c870
remolacho <jonathang_25@gmail.com>
2022-08-24 19:50:00 -0500

    was created test by factory filters

--- app/services/rank/get.rb
+++ app/services/rank/get.rb
@@ -1,7 +1,11 @@
 module Rank
   class Get
 
-    def self.build(data)
+    def self.factory(data)
+      return Rank::Filters::Stars.new(data) if data[:kind] == 'stars'
+      return Rank::Filters::Forks.new(data) if data[:kind] == 'forks'
+      return Rank::Filters::Languages.new(data) if data[:kind] == 'languages'
+
       raise NotImplementedError, 'you can not implement this search'
     end
 
--- spec/services/rank/get_spec.rb
+++ spec/services/rank/get_spec.rb
@@ -5,7 +5,27 @@ require 'rails_helper'
 describe Rank::Get, type: :service do
   context 'Validate allowed instances' do
     it 'the instance not found' do
-      expect{Rank::Get.build({kind: "test"})}.to raise_error(NotImplementedError)
+      expect{Rank::Get.factory({kind: "test"})}.to raise_error(NotImplementedError)
+    end
+
+    it 'The rank filter stars' do
+      data = {kind: "stars", field: 'language', item: 'JavaScript',  top: 2 }
+      service = Rank::Get.factory(data)
+      expect(service.kind_of? Rank::Filters::Stars).to eq true
+
+    end
+
+    it 'The rank filter forks' do
+      data = {kind: "forks", field: 'language', item: 'JavaScript',  top: 2 }
+      service = Rank::Get.factory(data)
+      expect(service.kind_of? Rank::Filters::Forks).to eq true
+
+    end
+
+    it 'The rank filter languages JavaScript' do
+      data = {kind: "languages", field: 'language', item: 'JavaScript',  top: 2 }
+      service = Rank::Get.factory(data)
+      expect(service.kind_of? Rank::Filters::Languages).to eq true
     end
   end
 end
